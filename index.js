57402d4a4babf21190826d44942a0302a5cc5969
soyuka <soyuka@gmail.com>
2014-09-03 23:49:02 +0200

    Added RPC + close on exception

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var fs = require('fs')
   , logger = require('ezseed-logger')('watcher')
   , debug = require('debug')('ezseed:watcher')
   , chalk = require('chalk')
+  , rpc = require('pm2-axon-rpc')
   , axon = require('axon')
+  , rep = axon.socket('rep')
   , sock = axon.socket('push')
+  , watcher
 
 module.exports = function(options, cb) {
 	
@@ -12,13 +15,17 @@ module.exports = function(options, cb) {
     options.root = options.root || __dirname
     options.tmp = options.tmp || p.join(options.root, '/tmp')
     options.socket = options.socket || 'unix://'+options.root+'/ezseed.sock'
+    options.rpc_socket = options.rpc_socket || 'unix://'+options.root+'/ezseed_rpc.sock'
 
     options.lang = options.lang || 'en'
 
     options.path = {}
-    options.path.relative = options.home ? p.relative(__dirname, options.home) : p.relative(__dirname, p.resolve(__dirname, './test/fixtures/watch'))
-    options.path.absolute = options.home ? p.resolve(__dirname, options.path.relative) : options.path.relative //tests only
 
+    if(!options.home)
+      throw new Error('Ezssed home directory is not defined')
+
+    options.path.relative = p.relative(__dirname, options.home) 
+    options.path.absolute = p.resolve(__dirname, options.path.relative) 
     options.db = options.db || {}
 
     var watcher_options = {
@@ -37,15 +44,19 @@ module.exports = function(options, cb) {
 
     require('ezseed-database')(options.db, function(){
 
+      var server = new rpc.Server(rep)
+      rep.bind(process.ezseed_watcher.rpc_socket)
+
       sock.bind(process.ezseed_watcher.socket)
 
       sock.on('bind', function() {
       
         debug('Socket bind on %s', process.ezseed_watcher.socket)
         watcher_options.socket = sock
+        watcher_options.rpc = server
         
         debug('Launching watcher on '+ options.path.absolute)
-        var watcher = require('./lib/watcher').watch(options.path.absolute, watcher_options)
+        watcher = require('./lib/watcher').watch(options.path.absolute, watcher_options)
 
         //optional callback
         if(cb) cb(null, watcher)
@@ -58,9 +69,25 @@ module.exports = function(options, cb) {
     process.on('SIGINT', function() {
       logger.info(chalk.bold.red("Caught interrupt signal"))
 
+      if(watcher) {
+        watcher.close()
+      }
+
       setTimeout(function() {
-              process.exit()
-          , 1000})
+        process.exit(0)
+      , 1000})
+    })
+
+    process.on('uncaughtException', function(e) {
+      if(watcher) {
+        watcher.close()
+      }
+
+      throw e;
+
+      setTimeout(function() {
+        process.exit(1)
+      , 1000})
     })
 
 }
